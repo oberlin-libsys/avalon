19572f561f75a37fe865760d12aa3e95f913a877
ryanwmichaud <92121030+ryanwmichaud@users.noreply.github.com>
2024-04-05 11:35:35 -0400

    merged w latest

--- app/models/user.rb
+++ app/models/user.rb
@@ -94,11 +94,7 @@ class User < ActiveRecord::Base
   def self.find_or_create_by_username_or_email(username, email, provider = 'local')
     find_and_verify_by_username(username) ||
       find_and_verify_by_email(email) ||
-<<<<<<< HEAD
-      create(username: email, email: email, password: Devise.friendly_token[0, 20], provider: provider)
-=======
       create(username: username, email: email, password: Devise.friendly_token[0, 20], provider: provider)
->>>>>>> latest/main
   end
 
   def self.from_api_token(token)
