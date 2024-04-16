53c66f886735463bd556ec9e6164bc78dca67522
ryanwmichaud <ryanwilliammichaud@gmail.com>
2024-04-16 11:14:30 -0400

    okta back

--- app/models/user.rb
+++ app/models/user.rb
@@ -32,7 +32,9 @@ class User < ActiveRecord::Base
   devise_list = [ :database_authenticatable, :invitable, :omniauthable,
                   :recoverable, :rememberable, :trackable, :validatable ]
   devise_list << :registerable if Settings.auth.registerable
-  devise_list << { authentication_keys: [:login] }
+  # devise_list << { authentication_keys: [:login] } unless Settings.auth.configuration[0].provider
+  devise_list <<  { omniauth_providers: [:oktaoauth] } if ENV['OKTA_CLIENT_ID']
+  #devise_list << { authentication_keys: [:login] }
 
   devise(*devise_list)
 
