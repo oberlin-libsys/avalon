b9c337312411139dca5dd1651b70f2cf6503b990
oberlin-libsys <76058968+oberlin-libsys@users.noreply.github.com>
2026-04-29 13:34:52 -0400

    comment out devise_scope

--- config/routes.rb
+++ config/routes.rb
@@ -60,12 +60,12 @@ Rails.application.routes.draw do
   end
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks', sessions: 'users/sessions' }
   
-  devise_scope :user do
-    match '/users/auth/:provider', to: 'users/omniauth_callbacks#passthru', as: :user_omniauth_authorize, via: [:get, :post]
-    Avalon::Authentication::Providers.collect { |provider| provider[:provider] }.uniq.each do |provider_name|
-      match "/users/auth/#{provider_name}/callback", to: "users/omniauth_callbacks##{provider_name}", as: "user_omniauth_callback_#{provider_name}".to_sym, via: [:get, :post]
-    end
-  end
+ # devise_scope :user do
+ #   match '/users/auth/:provider', to: 'users/omniauth_callbacks#passthru', as: :user_omniauth_authorize, via: [:get, :post]
+ #   Avalon::Authentication::Providers.collect { |provider| provider[:provider] }.uniq.each do |provider_name|
+ #     match "/users/auth/#{provider_name}/callback", to: "users/omniauth_callbacks##{provider_name}", as: "user_omniauth_callback_#{provider_name}".to_sym, via: [:get, :post]
+ #   end
+ # end
 
   mount BrowseEverything::Engine => '/browse'
 
